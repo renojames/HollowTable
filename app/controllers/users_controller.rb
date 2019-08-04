aceff6e497cdb54f18bf856399ebb2f95895d7a4
renojames <renojames19@gmail.com>
2019-08-04 16:41:54 -0700

    Established sigup, homepage
    Created signup functionality, homepage shell

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,4 +8,50 @@ class UsersController < ApplicationController
     end
   end
 
+  post '/signup' do
+    if entered_username? && entered_email? && entered_password?
+      @user = User.create(username: params["username"], email: params["email"], password: params["password"])
+      session["user_id"] = @user.id 
+      redirect '/home'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/home' do
+    if Helpers.is_logged_in?(session)
+      @user = Helpers.current_user(session)
+      erb :"/users/home"
+    else
+      redirect '/'
+    end
+  end
+  
+  
+  private
+
+  def entered_username?
+    if !!params["username"] && params["username"] != ""
+      true
+    else
+      false
+    end
+  end
+
+  def entered_email?
+    if !!params["email"] && params["email"] != ""
+      true
+    else
+      false
+    end
+  end
+
+  def entered_password?
+    if !!params["password"] && params["password"] != ""
+      true
+    else
+      false
+    end
+  end
+
 end
