8e803558c1cdf7b92a3a1d32ea8f6b4178d03350
renojames <renojames19@gmail.com>
2019-09-05 18:24:41 -0700

    Stubbed out games controller
    Stubbed out games controller

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,6 @@
+class GamesController < ApplicationController
+
+  # TO DO: Build out C.R.U.D for games. Do publishers, types, and genres need their own controllers, 
+  # or just built in validation?
+
+end
