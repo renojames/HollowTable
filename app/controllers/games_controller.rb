b0cf74cf7b9076c3c1ea48869326eb324e10fce1
renojames <renojames19@gmail.com>
2019-09-08 14:01:26 -0700

    Created create game route, w basic validation for title and publisher
    Created create game route, w basic validation for title and publisher

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -19,4 +19,17 @@ class GamesController < ApplicationController
     end
   end
 
+  post '/games' do
+    if !Game.find_by(name: params["name"])
+      game = Game.create(name: params["name"], summary: params["summary"], play_time: params["play_time"], max_players: params["max_players"], year_published: params["year_published"])
+      if !Publisher.find_by(name: params["publisher"])
+        game.publisher = Publisher.create(name: params["publisher"])
+      end
+      Helpers.current_user(session).games << game
+      redirect '/home'
+    else
+      redirect '/games/new'
+    end
+  end
+
 end
