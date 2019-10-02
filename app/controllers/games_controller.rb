83c3240e3b5ad714ccf95651a2fae2ef3fae0c30
renojames <renojames19@gmail.com>
2019-10-01 20:34:03 -0700

    Finished new game form basics
    Finished new game form basics; can now create a new game with new or existing genres and types.
    
    NOTE: Need to validate new genres and types for existing instances

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,13 +20,25 @@ class GamesController < ApplicationController
   end
 
   post '/games' do
-    binding.pry
     if !Game.find_by(name: params["name"])
       game = Game.create(name: params["name"], summary: params["summary"], play_time: params["play_time"], max_players: params["max_players"], year_published: params["year_published"])
       if !Publisher.find_by(name: params["publisher"])
         game.publisher = Publisher.create(name: params["publisher"])
       end
-      game.genres
+      if params["new_genre"] && params["new_genre"] != ""
+        genre = Genre.create(name: params["new_genre"])
+        game.genres << genre
+      end
+      params["genres"].each do |genre|
+        game.genres << Genre.find(genre)
+      end
+      if params["new_type"] && params["new_type"] != ""
+        type = Type.create(name: params["new_type"])
+        game.types << type
+      end
+      params["types"].each do |type|
+        game.types << Type.find(type)
+      end
       Helpers.current_user(session).games << game
       redirect '/home'
     else
