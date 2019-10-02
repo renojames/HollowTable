c7c8eabfb13fb82bea14c4a92fed8006cda36418
renojames <renojames19@gmail.com>
2019-10-01 20:20:45 -0700

    Fixed input for genre and type in new game form
    Fixed input for genre and type in new game form

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -20,11 +20,13 @@ class GamesController < ApplicationController
   end
 
   post '/games' do
+    binding.pry
     if !Game.find_by(name: params["name"])
       game = Game.create(name: params["name"], summary: params["summary"], play_time: params["play_time"], max_players: params["max_players"], year_published: params["year_published"])
       if !Publisher.find_by(name: params["publisher"])
         game.publisher = Publisher.create(name: params["publisher"])
       end
+      game.genres
       Helpers.current_user(session).games << game
       redirect '/home'
     else
