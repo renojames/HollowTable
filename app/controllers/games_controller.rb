663426bf61860e3e18b94b2f9da658fca665f26c
renojames <renojames19@gmail.com>
2020-01-25 13:01:41 -0800

    Finished update functionality
    Finished update functionality

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -69,6 +69,41 @@ class GamesController < ApplicationController
     erb :"/games/edit"
   end
 
+  patch '/games/:id' do
+    @game = Game.find(params[:id])
+    
+    @game.genres.clear
+    @game.types.clear
+
+    @game.update(name: params["name"], summary: params["summary"], play_time: params["play_time"], max_players: params["max_players"], year_published: params["year_published"])
+
+    if !!params["publisher_id"] && params["publisher_id"] != ""
+      @game.publisher = Publisher.find(params["publisher_id"])
+    end
+    if !Publisher.find_by(name: params["new_publisher"].strip!) && params["new_publisher"] != ""
+      @game.publisher = Publisher.create(name: params["new_publisher"])
+    end
+    if !!params["new_genre"] && params["new_genre"] != ""
+      @genre = Genre.create(name: params["new_genre"])
+      @game.genres << genre
+    end
+    if !!params["genres"]
+      params["genres"].each do |genre|
+        @game.genres << Genre.find(genre)
+      end
+    end
+    if !!params["new_type"] && params["new_type"] != ""
+      type = Type.create(name: params["new_type"])
+      @game.types << type
+    end
+    if !!params["types"]
+      params["types"].each do |type|
+        @game.types << Type.find(type)
+      end
+    end
+    erb :'/games/show'
+  end
+
   delete '/game/:id' do
     @game = Game.find(params[:id])
     if Helpers.current_user(session).games.include?(@game)
